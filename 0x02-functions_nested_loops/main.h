cb22fc19b6a737ed97643c8f307903312ea92eda
Eitharr <eitharalarabi20@gmail.com>
2023-09-12 12:32:10 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,5 @@
 #include main.h
 #define main.h
-
 int_putchar(char);
 void print_alphabet(void);
 void print_alphabet(void);
@@ -14,5 +13,4 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-
 #endif;
