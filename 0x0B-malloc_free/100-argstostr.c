ca398e5413f368d322d8f5171866086b1e9d0b57
Eitharr <eitharalarabi20@gmail.com>
2023-10-04 07:12:04 -0700

    malloc

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,7 +11,7 @@ char *argstostr(int ac, char **av)
 int i, n, r = 0, l = 0;
 char *str;
 if (ac == 0 || av == NULL)
-return(NULL);
+return (NULL);
 for (i = 0; i < ac; i++)
 {
 for (n = 0; av[i][n]; n++)
